907b4de50c5b21ea175317b5fdac9d3c6eae383d
Jan Smidrkal <jsmidrka@c2r10s3.42prague.com>
2023-10-27 19:13:34 +0200

    add corrected ft_isascii() and comment main()

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: jsmidrka <jsmidrka@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 17:01:39 by jsmidrka          #+#    #+#             */
-/*   Updated: 2023/10/27 19:07:43 by jsmidrka         ###   ########.fr       */
+/*   Updated: 2023/10/27 19:12:48 by jsmidrka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,15 @@ checks whether c is a 7-bit unsigned char value that fits into the ASCII charact
 #include <unistd.h>
 /*
 // int	ft_isascii(int ch)
-// {Ád
+// {
+// 	if (ch >= 0 && ch <= 127)
+// 		return (1);
+// 	else
+// 		return (0);
+// }
+
+0x7f is 127 in decimal
+& is bitwise operator and
 	Binary AND Operator copies a bit to the result if it exists in both operands.
 	(A & B) = 12, i.e., 0000 1100
 ~ Binary One's Complement Operator is unary and has the effect of 'flipping' bits.
@@ -30,6 +38,7 @@ int	ft_isascii(int c)
 {
 	return (!(c & ~0x7f));
 }
+
 /*
 int	main(void)
 {
