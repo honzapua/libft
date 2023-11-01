cbc503e6f9cd03b3f8e8631591354a7e845f3d84
Jan Smidrkal <jsmidrka@c2r7s7.42prague.com>
2023-11-01 20:21:48 +0100

    memcpy structure

---
+++ ft_memcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsmidrka <jsmidrka@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 19:55:29 by jsmidrka          #+#    #+#             */
+/*   Updated: 2023/11/01 19:59:35 by jsmidrka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+NAME
+memcpy - copy memory area
+
+SYNOPSIS
+#include <string.h>
+
+void *memcpy(void *dest, const void *src, size_t n);
+
+DESCRIPTION
+The memcpy() function copies n bytes from memory area src to memory area dest.
+The memory areas must not overlap.  Use memmove(3) if the memory areas do 
+overlap.
+
+RETURN VALUE
+The memcpy() function returns a pointer to dest.
+
+ATTRIBUTES
+For an explanation of the terms used in this section, see attributes(7).
+*/
+
+void	*memcpy(void *dest, const void *src, size_t n)
+{
+	while (src != '/0'* condition */)
+	{
+		/* code */
+	}
+	
+}
