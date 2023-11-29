27d7d2c975ceef85e40920d6990aa0f4ae1fb945
Jan Smidrkal <jsmidrka@c2r6s6.42prague.com>
2023-11-29 20:11:18 +0100

    template to finish

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jsmidrka <jsmidrka@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 21:43:01 by jsmidrka          #+#    #+#             */
-/*   Updated: 2023/11/28 21:45:40 by jsmidrka         ###   ########.fr       */
+/*   Updated: 2023/11/29 20:10:36 by jsmidrka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,21 @@ string, which is the result of the concatenation
 of ’s1’ and ’s2’.
 
 */
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*strall;
+
+	if (!s1 || !s2)
+		return (NULL);
+	
+	while (*s1 != '\0')
+	{
+		*strall++ = *s1++;
+	}
+
+	while (*s2 != '\0')
+	{
+		*(strall -1)++ = *s2++;
+	}
+	return (strall);
+}
