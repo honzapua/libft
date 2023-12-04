ecec4b462f99ffdb35955164b6f4532559245a9b
Jan Smidrkal <jsmidrka@c2r5s3.42prague.com>
2023-12-04 20:10:13 +0100

    comments ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,28 +6,38 @@
 /*   By: jsmidrka <jsmidrka@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 18:55:51 by jsmidrka          #+#    #+#             */
-/*   Updated: 2023/12/04 19:52:13 by jsmidrka         ###   ########.fr       */
+/*   Updated: 2023/12/04 20:09:11 by jsmidrka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+/*
+If n is zero, returns 2. This is because the string representation of zero
+requires two characters: one for the digit '0' and one for the null terminator.
+
+If n is negative, increments res by 1. This is to account for the additional 
+'-' character in the string representation of negative numbers.
+
+Counts the number of digits in the absolute value of n. The loop increments
+res for each digit until n becomes zero after successive division by 10.
+*/
 static unsigned int	get_size(int n)
 {
-	unsigned int	size;
+	unsigned int	size_str;
 
-	size = 1;
+	size_str = 1;
 	if (n == 0)
 		return (2);
 	if (n < 0)
-		++size;
+		++size_str;
 	while (n != 0)
 	{
-		++size;
+		++size_str;
 		n /= 10;
 	}
-	return (size);
+	return (size_str);
 }
 
 /*
@@ -51,34 +61,44 @@ Description:
 Allocates (with malloc(3)) and returns a string
 representing the integer received as an argument.
 Negative numbers must be handled
+
+final_str[--size_str] = '\0';
+Sets the last character of the string to the null terminator.
+
+else if (n < 0 && final_str)
+If n is negative, sets the first character of the string to '-' and then
+ fills in the remaining digits of the absolute value of n in reverse order.
+
+while (n > 0 && final_str)
+fills in the remaining digits of n in reverse order.
 */
 char	*ft_itoa(int n)
 {
-	unsigned int	size;
+	unsigned int	size_str;
 	char			*final_str;
 
-	size = get_size(n);
-	final_str = malloc(size * sizeof(char));
+	size_str = get_size(n);
+	final_str = malloc(size_str * sizeof(char));
 	if (final_str == NULL)
 		return (final_str);
-	final_str[--size] = '\0';
+	final_str[--size_str] = '\0';
 	if (n == 0 && final_str)
 		final_str[0] = '0';
 	else if (n < 0 && final_str)
 	{
 		final_str[0] = '-';
-		final_str[--size] = '0' - (n % 10);
+		final_str[--size_str] = '0' - (n % 10);
 		n = -(n / 10);
 	}
 	while (n > 0 && final_str)
 	{
-		final_str[--size] = '0' + (n % 10);
+		final_str[--size_str] = '0' + (n % 10);
 		n /= 10;
 	}
 	return (final_str);
 }
-
 /*
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
